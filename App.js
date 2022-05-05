e578666edfe5f22c18167bc1b3e176e74f536fef
JoanaLuzz <95249054+JoanaLuzz@users.noreply.github.com>
2022-05-05 15:57:39 +0100

    nova cena

--- App.js
+++ App.js
@@ -4,10 +4,10 @@ import Slider from "./components/Slider";
 import NfcManager, { NfcTech } from "react-native-nfc-manager";
 
 const images = [
-  "https://images.pexels.com/photos/11485306/pexels-photo-11485306.jpeg?cs=srgb&dl=pexels-anna-rye-11485306.jpg&fm=jpg",
-  "https://images.pexels.com/photos/11183928/pexels-photo-11183928.jpeg?cs=srgb&dl=pexels-angelica-reyn-11183928.jpg&fm=jpg",
-  "https://images.pexels.com/photos/11548856/pexels-photo-11548856.jpeg?cs=srgb&dl=pexels-azra-tuba-demir-11548856.jpg&fm=jpg",
-  "https://images.pexels.com/photos/11163068/pexels-photo-11163068.jpeg?cs=srgb&dl=pexels-gaspar-zaldo-11163068.jpg&fm=jpg0",
+  "https://i.imgur.com/SHC9Y3K.png",
+  "https://i.imgur.com/MIZMAjZ.png",
+  "https://i.imgur.com/pcjsiRU.png",
+  "https://i.imgur.com/DMwACUi.png",
 ];
 
 export default class App extends React.Component {
