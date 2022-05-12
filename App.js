328ea012a920e6a8c8094117bb238ab673b1cdb3
Scuffedwrldwide <kiko2004kiko@gmail.com>
2022-05-12 12:54:50 +0100

    Revert "caralho"
    This reverts commit 3605d6223133a98c2314eb2485497133e8cc6569.

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-/* eslint-disable prettier/prettier */
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import Routes from './src1/routes';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    return (
-        <NavigationContainer>
-            <Routes/>
-        </NavigationContainer>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app, luv!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
